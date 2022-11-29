5175fb5e2537298bcce6305dfe5182d86951da12
nalinsb <nalinsanjeewa1997@gmail.com>
2022-11-29 14:07:48 +0530

    do userDAOImpl

--- src/main/java/lk/ijse/dep9/dao/CrudDAO.java
+++ src/main/java/lk/ijse/dep9/dao/CrudDAO.java
@@ -3,14 +3,15 @@ package lk.ijse.dep9.dao;
 import lk.ijse.dep9.entity.SuperEntity;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface CrudDAO<T extends SuperEntity, ID extends Serializable> extends SuperDAO {
 
-    long count();
+    long count() throws SQLException;
 
-    void deleteById(ID PK) ;
+    void deleteById(String user_name) throws SQLException;
 
     boolean existsById(ID PK);
 
