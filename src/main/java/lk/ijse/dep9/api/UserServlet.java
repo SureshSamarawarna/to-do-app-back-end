c0f1d35bdef1d5d973b8dfe4057e8174de245345
nalinsb <nalinsanjeewa1997@gmail.com>
2022-11-29 12:53:22 +0530

    create connection pool

--- src/main/java/lk/ijse/dep9/api/UserServlet.java
+++ src/main/java/lk/ijse/dep9/api/UserServlet.java
@@ -1,13 +1,20 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
 @WebServlet(name = "UserServlet", value = "/UserServlet")
 public class UserServlet extends HttpServlet {
+
+    @Resource(lookup = "java:comp/env/jdbc/dep9-todo")
+    private DataSource pool;
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
